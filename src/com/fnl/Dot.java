6047f793e1c02820e61ab7216fe6da1504c435a0
gabderakhmanov <ugla87@yandex.ru>
2017-11-01 22:44:20 +0300

    WRONG init for final field

---
+++ src/com/fnl/Dot.java
@@ -0,0 +1,28 @@
+package com.fnl;
+
+public class Dot {
+
+    private final int x;
+    private int y;
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
